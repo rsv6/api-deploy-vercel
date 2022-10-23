8b0cd0d034891ec9b3ea88134fc69e97626c38fd
Rafael Santos <rafaelsvbr@gmail.com>
2022-10-23 14:09:02 -0300

    add: variable interpolation in alert, adicition message in html

--- api/produto.js
+++ api/produto.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 
 router.get("/", (req, res) => {
+  const nome = "rsv6"
+
   try {
     // res.json({
     //   status: 200,
     //   message: "Get data successfully 213213",
     // });
 
-    res.status(200).send("<center><strong>Success</strong></center>");
+    res.status(200).send(`<center><strong><button onclick="alert('Development by ${nome}');">Welcome at page teste, deploy back in NodeJS on Vercel</strong></center>`);
 
   } catch (err) {
     console.log(err);
